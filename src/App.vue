f39212fffb957971702000d96f51703350caeb3d
AlinaMollinedo <alina.mollinedo@ucb.edu.bo>
2025-09-13 22:34:12 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<script setup>
+import { ref } from 'vue'
+import Lineal from '../src/components/Lineal.vue'
+import Multiplicativo from '../src/components/Multiplicativo.vue'
+
+const algs = ref(null)
+
+</script>
+
+<template>
+  <v-app class="bg">
+    <v-main>
+      <v-container>
+        <v-row class="mt-8">
+          <v-col cols="12">
+            <p class="title">Generador de Números Pseudoaleatorios</p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-container class="container">
+            <v-tabs v-model="algs">
+              <v-tab class="tab" value="lineal" :class="{'reversed-tab': algs === 'lineal'}">Algoritmo Lineal</v-tab>
+              <v-tab class="tab" value="multiplicativo" :class="{'reversed-tab': algs === 'multiplicativo'}">Algoritmo Multiplicativo</v-tab>
+            </v-tabs>
+            <v-tabs-window v-model="algs">
+              <v-tabs-window-item value="lineal">
+                <Lineal/>
+              </v-tabs-window-item>
+              <v-tabs-window-item value="multiplicativo">
+                <Multiplicativo/>
+              </v-tabs-window-item>
+            </v-tabs-window>
+          </v-container>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer class="footer order-last">
+      <div class="flex-1-0-100 text-center mt-2">
+        {{ new Date().getFullYear() }} — <strong>Alina Mollinedo Dávila</strong>
+      </div>
+    </v-footer>
+  </v-app>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Patrick+Hand&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap');
+
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
+.v-main{
+  flex-direction: column;
+}
+
+.bg{
+  background: linear-gradient(0deg, rgb(196, 73, 0, 0.25), rgb(196, 73, 0, 0.1));
+}
+
+.v-container{
+  flex: 1;
+}
+
+.title {
+  font-family: 'Sour Gummy', sans-serif;
+  font-size:xx-large;
+  font-weight: 700;
+  color: #04151F;
+}
+
+.v-tabs{
+  border-bottom: 5px solid #183A37;
+}
+
+.tab{
+  font-family: 'Patrick Hand', cursive;
+  font-weight: 500;
+  font-size: larger;
+  position: relative;
+  transition: color 1s ease;
+  background-color: transparent;
+  color: #183A37;
+}
+
+.reversed-tab{
+  background-color: #183A37;
+  color: #f8eedd;
+  top: 100%;
+  animation: fillUp 0.3s ease-out forwards;
+}
+
+@keyframes fillUp {
+	10% {
+		top: 100%;
+	}
+	100% {
+		top: 0%;
+	}
+}
+
+.footer{
+  background-color: #183A37;
+  color: #f8eedd;
+  width: 100%;
+  max-height: 100px;
+  padding: 10px 0;
+}
+
+</style>
---
+++ src/components/Multiplicativo.vue
@@ -0,0 +1,286 @@
+<script setup>
+import { ref } from 'vue'
+
+const calcA = ref([
+    {id: 1, formula: 'a = 3 + 8k'},
+    {id: 2, formula: 'a = 5 + 8k'}
+])
+
+const isValid = ref(false)
+const submitted = ref(false)
+
+const P = ref(null)
+const x0 = ref(null)
+const k = ref(null)
+const ca = ref(1)
+const g = ref(null)
+const m = ref(null)
+const a = ref(null)
+
+const xi = ref([])
+const ri = ref([])
+
+const Pcopy = ref(null)
+const range = (n) => Array.from({ length: n }, (_, i) => i + 1)
+const digits = ref(3)
+
+const calc = () => {
+    submitted.value = true
+
+    P.value = parseInt(P.value)
+    x0.value = parseInt(x0.value)
+    k.value = parseInt(k.value)
+
+    g.value = Math.ceil((Math.log10(P.value) / Math.log10(2)) + 2)
+    m.value = Math.pow(2, g.value)
+    a.value = (ca.value == 1) ? 3 + 8 * k.value : 5 + 8 * k.value
+
+    xi.value = []
+    ri.value = []
+    xi.value.push(x0.value)
+
+    for(let i = 1; i <= P.value; i++){
+        xi.value.push((a.value * xi.value.at(i - 1)) % m.value)
+        ri.value.push(xi.value.at(i) / (m.value - 1))
+    }
+
+    Pcopy.value = P.value
+}
+
+const clear = () => {
+    isValid.value = false
+    submitted.value = false
+
+    P.value = null
+    x0.value = null
+    k.value = null
+    c.value = null
+    g.value = null
+    m.value = null
+    a.value = null
+
+    xi.value = []
+    ri.value = []
+}
+
+const lessDigits = () => {
+    digits.value--
+}
+
+const moreDigits = () => {
+    digits.value++
+}
+
+</script>
+
+<template>
+    <v-form v-model="isValid" @submit.prevent="calc()">
+        <v-row class="mt-5">
+            <v-col cols="12" sm="4">
+                <v-text-field
+                class="input"
+                v-model="P"
+                label="Periodo"
+                type="number"
+                :rules="[
+                    v => !!v || 'Se requiere un periodo',
+                    v => (Number.isInteger(parseFloat(v)) && v > 0) || 'El periodo debe ser un número entero positivo'
+                ]"
+                required
+                />      
+            </v-col>
+            <v-col cols="12" sm="4">
+                <v-text-field
+                class="input"
+                v-model="x0"
+                label="Semilla (X₀)"
+                type="number"
+                :rules="[
+                    v => !!v || 'Se requiere una semilla.',
+                    v => (Number.isInteger(parseFloat(v)) && v > 0) || 'La semilla debe ser un número entero positivo',
+                    v => v % 2 != 0 || 'La semilla debe ser un número impar'
+                ]"
+                required
+                />  
+            </v-col>
+            <v-col cols="12" sm="2">
+                <v-text-field
+                class="input"
+                v-model="k"
+                label="k"
+                type="number"
+                :rules="[
+                    v => !!v || 'Se requiere la constante k',
+                    v => (Number.isInteger(parseFloat(v)) && v > 0) || 'La constante k debe ser un número entero positivo'
+                ]"
+                required
+                /> 
+            </v-col>
+            <v-col cols="12" sm="2">
+                <v-select
+                class="input"
+                v-model="ca"
+                :items="calcA"
+                item-title="formula"
+                item-value="id"
+                label="Cálculo de a"
+                required
+                />
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="6">
+                <v-btn
+                class="btn"
+                variant="elevated"
+                @click="calc"
+                :disabled="!isValid"
+                block>
+                    Generar
+                </v-btn>
+            </v-col>
+            <v-col cols="6">
+                <v-btn
+                class="btn"
+                variant="elevated"
+                @click="clear"
+                :disabled="!submitted"
+                block>
+                    Limpiar
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-form>
+    <v-row class="my-8 justify-center" v-if="submitted">
+        <v-col cols="12" sm="3">
+            <v-card>
+                <v-card-text class="card-title">Constantes calculadas</v-card-text>
+            </v-card>
+        </v-col>
+        <v-col cols="12" sm="3">
+            <v-card>
+                <v-card-text class="card-text text-center">g = {{ g }}</v-card-text>
+            </v-card>
+        </v-col>
+        <v-col cols="12" sm="3">
+            <v-card>
+                <v-card-text class="card-text text-center">m = {{ m }}</v-card-text>
+            </v-card>
+        </v-col>
+        <v-col cols="12" sm="3">
+            <v-card>
+                <v-card-text class="card-text text-center">a = {{ a }}</v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
+    <v-row class="px-2 pb-12" v-if="submitted">
+        <v-col cols="12">
+            <v-table class="elevation-1" striped="even">
+                <thead>
+                    <tr>
+                        <th class="header">i</th>
+                        <th class="header">Operación</th>
+                        <th class="header">X<sub>i</sub></th>
+                        <th class="header">
+                            r<sub>i</sub>
+                            <v-btn
+                            class="ml-3"
+                            icon
+                            size="30"
+                            color="#183A37"
+                            @click="lessDigits"
+                            >
+                            <v-icon size="20">mdi-chevron-left</v-icon>
+                            </v-btn>
+                            <v-btn
+                            class="ml-2"
+                            icon
+                            size="30"
+                            color="#183A37"
+                            @click="moreDigits"
+                            >
+                            <v-icon size="20">mdi-chevron-right</v-icon>
+                            </v-btn>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                    v-for="i in range(Pcopy)"
+                    :key="i"
+                    >
+                        <td class="table-content">{{ i }}</td>
+                        <td class="table-content">({{ a }} * {{ xi.at(i - 1) }}) MOD({{ m }})</td>
+                        <td class="table-content">{{ xi.at(i) }}</td>
+                        <td class="table-content">{{ ri.at(i - 1).toFixed(digits) }}</td>
+                    </tr>
+                </tbody>
+            </v-table>
+        </v-col>
+    </v-row>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Funnel+Display:wght@300..800&display=swap');
+
+.input{
+    font-family: 'Funnel Display', sans-serif;
+    font-size: large;
+    color: #183A37;
+}
+
+.btn{
+    font-family: 'Funnel Display', sans-serif;
+    font-size: larger;
+    background-color: transparent;
+    color: #432534;
+    border: 2px solid #432534;
+    transition: all 0.5s ease;
+}
+
+.btn:disabled{
+    background-color: transparent;
+    color: rgb(67, 37, 52, 0.4);
+    border: 2px solid rgb(67, 37, 52, 0.4);
+}
+
+.btn:hover{
+    background-color: #432534;
+    color: #f8eedd;
+}
+
+.v-card{
+    background-color: rgb(255, 242, 235, 1);
+}
+
+.card-title{
+    font-family: 'Funnel Display', sans-serif;
+    font-weight: 600;
+    font-size: larger;
+}
+
+.card-text{
+    font-family: 'Funnel Display', sans-serif;
+    font-weight: 400;
+    font-size: large;
+}
+
+.v-table{
+    background-color: #fff2eb;
+    border-radius: 10px;
+}
+
+.header{
+    font-family: 'Funnel Display', sans-serif;
+    font-size: large;
+    background-color: #183A37;
+    color: #f8eedd;
+}
+
+.table-content{
+    font-family: 'Funnel Display', sans-serif;
+    font-weight: 300;
+    font-size: medium;
+}
+
+</style>
